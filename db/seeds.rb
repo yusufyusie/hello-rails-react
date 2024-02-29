ae303ee20c443bc82c9394ef060fb2a734864387
yusufyusie <yesuf023@gmail.com>
2024-02-29 16:55:16 +0300

    Seed the database with 5 different greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Greeting.create(message: "Hello")
+Greeting.create(message: "Hi")
+Greeting.create(message: "Hey")
+Greeting.create(message: "Hola")
+Greeting.create(message: "Bonjour")
