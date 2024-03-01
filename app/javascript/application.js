a1ef97c7dc562e2e109d3ba68c7cd202213feaed
yusufyusie <yesuf023@gmail.com>
2024-03-01 23:11:15 +0300

    Wrap your App component with the Provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
+import App from '../components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.body.appendChild(document.createElement('div')),
+  );
+});
